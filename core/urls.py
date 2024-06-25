4a46b89e47fdb2840e9f672b3b5088a177cf1bba
Akmaljon701 <akmalkhanever@gmail.com>
2024-06-25 15:25:48 +0500

    updated

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from utils.enums import talon_number_choices
+from utils.enums import tallon_number_choices
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
 
-    path('enum/talon_numbers/', talon_number_choices, name='talon_number_choices'),
+    path('enum/talon_numbers/', tallon_number_choices, name='talon_number_choices'),
 
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
--- utils/enums.py
+++ utils/enums.py
@@ -2,14 +2,14 @@ from django.http import JsonResponse
 from drf_spectacular.utils import extend_schema
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
-from talon.models import Talon
+from api.models import Tallon
 
 
 @extend_schema(responses={
-    200: {"description": "The operation was completed successfully", "example": dict(Talon.TALON_NUMBER_CHOICES)},
+    200: {"description": "The operation was completed successfully", "example": dict(Tallon.TALON_NUMBER_CHOICES)},
 })
 @api_view(['GET'])
 @permission_classes([AllowAny])
-def talon_number_choices(request):
-    choices = dict(Talon.TALON_NUMBER_CHOICES)
+def tallon_number_choices(request):
+    choices = dict(Talon.TALLON_NUMBER_CHOICES)
     return JsonResponse(choices)
