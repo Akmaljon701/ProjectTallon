2205226fc71ae3f7af725d0635b0bbc03fe07e11
Akmaljon701 <akmalkhanever@gmail.com>
2024-07-01 11:57:11 +0500

    updated

--- user/urls.py
+++ user/urls.py
@@ -3,7 +3,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from user.views import create_user, update_user, get_users, get_current_user, custom_token_obtain_pair
 
 urlpatterns = [
-    path('login/', custom_token_obtain_pair, name='token_obtain_pair'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('create/', create_user, name='create_user'),
     path('update/', update_user, name='update_user'),
